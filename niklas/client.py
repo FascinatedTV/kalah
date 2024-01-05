ce965552209cdbd0c330eab628ad2d8e3cc341f6
Niklas Haas <fascinated.tv.2002@gmail.com>
2024-01-05 10:31:33 +0100

    negamax try - not finished

--- niklas/client.py
+++ niklas/client.py
@@ -7,46 +7,54 @@ import hashlib
 CLIENT_SIDE = kgp.SOUTH
 DEPTH = 5
 
-def evaluate(state : kgp.Board) -> int:
+def evaluate(state : kgp.Board, side) -> int:
     # return sum(state.south_pits) - sum(state.north_pits) + 2 * (state.south - state.north)
-    return state[kgp.SOUTH] - state[kgp.NORTH]
+    return state[side] - state[not side]
 
 def search(state : kgp.Board, depth : int, side : bool, alpha = -math.inf, beta = math.inf) -> (int, int):
     """
     Searches the best move to a given board state\n
     Returns a move and its evaluation
     """
-    returns = []
+    maxScore = -1 * math.inf
+    bestMove = -1
     for move in state.legal_moves(side):
         if depth <= 0:
-            returns.append((evaluate(state), move))
+            if (score := evaluate(state, side)) > maxScore:
+                maxScore, bestMove = score, move
             continue
 
         new_board, repeat_move = state.sow(side, move)
         if new_board.is_final():
-            returns.append((evaluate(new_board), move))
+            if (score := evaluate(new_board, side)) > maxScore:
+                maxScore, bestMove = score, move
             continue
-
-        # Depth search
-        result = search(
-            state = new_board,
-            depth = depth - 1,
-            side  = repeat_move == side, # XNOR
-            alpha = alpha,
-            beta  = beta
-        )
-        returns.append((result[0], move))
-
-        if side: # Min Player
-            if result[0] < alpha: break
-            beta = min(beta, result[0])
-        else: # Max Player
-            if result[0] > beta: break
-            alpha = max(alpha, result[0])
         
-    if side:
-        return min(returns, key=lambda x: x[0])
-    return max(returns, key=lambda x: x[0])
+        result = 0
+        if repeat_move:
+            result = -search(
+                state = new_board,
+                depth = depth - 1,
+                side  = side,
+                alpha = alpha,
+                beta  = beta
+            )[0]
+        else:
+            result = -search(
+                state = new_board,
+                depth = depth - 1,
+                side  = not side,
+                alpha = -beta,
+                beta  = -alpha
+            )[0]
+        if result > maxScore:
+                maxScore, bestMove = result, move
+
+        if result > beta: break
+        alpha = max(alpha, result)
+
+    return maxScore, bestMove
+    
     
     
 def agent(state : kgp.Board):
